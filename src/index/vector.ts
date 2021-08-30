3e94bbc36c70caab701a811609b88a52220eb9c0
XiaoWinter <3388656601@qq.com>
2021-08-30 16:22:58 +0800

    [imp]:编写向量的部分测试用例

--- src/index/vector.ts
+++ src/index/vector.ts
@@ -3,12 +3,19 @@ import { getMaxMin, tuz } from "../utils";
 import MatrixImpl from "./matrix";
 
 export default class VectorImpl implements Vector {
-  value: number[];
+  #value: number[];
   constructor(...args: number[] | number[][]) {
-    this.value = args.flat(Infinity) as number[];
+    this.#value = args.flat(Infinity) as number[];
   }
 
-  toString = () => {
+  get value() {
+    return this.#value;
+  }
+  private set value(value: number[]) {
+    this.#value = value;
+  }
+
+  toString() {
     const [max, min] = getMaxMin(this.value);
 
     const maxStrLength = Math.max(("" + max).length, ("" + min).length);
@@ -18,18 +25,18 @@ export default class VectorImpl implements Vector {
       pre += `|${" ".repeat(repeatTime)}${cur}|\n`;
       return pre;
     }, "");
-  };
+  }
 
-  addition = (vector: Vector) => {
+  addition(vector: Vector) {
     const length = Math.max(this.value.length, vector.value.length);
     let result: number[] = [];
     for (let i = 0; i < length; i++) {
       result[i] = tuz(this.value[i]) + tuz(vector.value[i]);
     }
     return new VectorImpl(result);
-  };
+  }
 
-  subtraction = (vector: Vector) => {
+  subtraction(vector: Vector) {
     const length = Math.max(this.value.length, vector.value.length);
     let result: number[] = [];
 
@@ -37,25 +44,25 @@ export default class VectorImpl implements Vector {
       result[i] = tuz(this.value[i]) - tuz(vector.value[i]);
     }
     return new VectorImpl(result);
-  };
+  }
 
-  multiplication = (scalar: number) => {
+  multiplication(scalar: number) {
     let result: number[] = this.value.map((i) => scalar * i);
     return new VectorImpl(result);
-  };
+  }
 
-  dotted = (vector: Vector) => {
+  dotted(vector: Vector) {
     const length = Math.min(this.value.length, vector.value.length);
     let result = 0;
     for (let i = 0; i < length; i++) {
       result += this.value[i] * vector.value[i];
     }
     return result;
-  };
+  }
 
-  toMatrix = () => {
+  toMatrix() {
     return new MatrixImpl();
-  };
+  }
 
   /**
    * @description:
@@ -67,7 +74,7 @@ export default class VectorImpl implements Vector {
    * cz = axby − aybx
    *
    */
-  crossProduct = (vector: Vector) => {
+  crossProduct(vector: Vector) {
     let result: number[] = [];
     result[0] =
       this.value[1] * vector.value[2] - this.value[2] * vector.value[1];
@@ -76,9 +83,9 @@ export default class VectorImpl implements Vector {
     result[2] =
       this.value[0] * vector.value[1] - this.value[1] * vector.value[0];
     return new VectorImpl(result);
-  };
+  }
 
-  lineTrans = (matrix: Matrix) => {
+  lineTrans(matrix: Matrix) {
     return new VectorImpl();
-  };
+  }
 }
--- tests/index.ts
+++ tests/index.ts
@@ -2,10 +2,69 @@ import VectorImpl from "../src/index/vector";
 import { test } from "../decorator/index";
 
 class Test {
-  @test
+  // @test
   BuildVector() {
     new VectorImpl(1, 2, 3);
     new VectorImpl();
+  }
+
+  // @test
+  toString() {
+    console.log(new VectorImpl(1, 2, 3).toString());
+  }
+
+  // @test
+  addition() {
+    const v1 = new VectorImpl(1, 2, 3);
+    const v2 = new VectorImpl(4, 4, 3);
+    console.log(v1.toString());
+    console.log(v1.toString());
+    console.log(v1.addition(v2).toString());
+  }
+
+  // @test
+  subtraction() {
+    const v1 = new VectorImpl(5, 4, 3);
+    const v2 = new VectorImpl(1, 1, 1);
+    console.log(v1.toString());
+    console.log(v2.toString());
+    console.log(v1.subtraction(v2).toString());
+  }
+
+  // @test
+  multiplication() {
+    const v1 = new VectorImpl(1, 3, 2);
+    console.log(v1.toString());
+    console.log(v1.multiplication(2).toString());
+  }
+
+  // @test
+  dottedLine() {
+    const v1 = new VectorImpl(0, 0, 3);
+    const v2 = new VectorImpl(0, 1, 0);
+
+    console.log(v1.dotted(v2));
+  }
+  @test
+  dottedSameLine() {
+    const v1 = new VectorImpl(0, 0, 2);
+    const v2 = new VectorImpl(0, 0, 2);
+
+    console.log(v1.dotted(v2));
+  }
+
+  /**
+   * @description: 单位叉积测试 ,004
+   * @param {*}
+   * @return {*}
+   */
+  @test
+  crossProductUnit() {
+    const v1 = new VectorImpl(2, 0, 0);
+    const v2 = new VectorImpl(0, 2, 0);
+    console.log(v1.toString());
+    console.log(v2.toString());
+    console.log(v1.crossProduct(v2).toString());
   }
 }
 
